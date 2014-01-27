9ab002d1f1c03f8a1595a9748e0e9a69afbdb2e8
Lalo Martins <lalo.martins@gmail.com>
2014-01-27 14:06:30 +0100

    bugfix: easier global install

--- mts.sh
+++ mts.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+LIBDIR="$(dirname $(dirname $0))/lib/node_modules"
+if ! echo "$NODE_PATH" | grep -q "$LIBDIR"; then
+    if test -z "$NODE_PATH"; then
+        NODE_PATH="$LIBDIR"
+    else
+        NODE_PATH="$NODE_PATH:$LIBDIR"
+    fi
+    export NODE_PATH
+fi
+
 if which nodejs > /dev/null; then
     nodejs -e 'require("meteor-shower").main()'
 else if which node > /dev/null; then
